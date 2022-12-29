cb832b81fcb2f8bd0f063ea5fe561d37803de156
jsburbano <jhoan@aplazo.mx>
2022-12-29 01:26:04 -0500

    add: connection to mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: `mongodb+srv://${config.get<string>(
+          'MONGO.USER',
+        )}:${config.get<string>(
+          'MONGO.PASSWORD',
+        )}@burbanocorp.91g2g.mongodb.net/${config.get<string>(
+          'MONGO.DATABASE',
+        )}?retryWrites=true&w=majority`,
+        useNewUrlParser: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,26 @@
+const environment = ()=> ({
+    AWS: {
+        BUCKET: process.env.NODE_AWS_BUCKET,
+        BUCKET_REGION: process.env.NODE_AWS_BUCKET_REGION,
+        PUBLIC_ACCESS_KEY: process.env.NODE_AWS_PUBLIC_ACCESS_KEY,
+        SECRET_KEY: process.env.NODE_AWS_SECRET_KEY,
+        QR_PATH: 'images/qr-codes/'
+    },
+    CLIENT:{
+        WEB_APP_URL: process.env.NODE_CLIENT_WEB_APP_URL,
+        VIEW_INVITE: process.env.NODE_CLIENT_STATUS_INVITE
+    },
+    MONGO: {
+        USER: process.env.NODE_MONGO_USER,
+        PASSWORD: process.env.NODE_MONGO_PASSWORD,
+        DATABASE: process.env.NODE_MONGO_DATABASE,
+    }
+})
+
+export default environment;
+
+export function getAWSPATH(NAME:string){
+    const {AWS:{BUCKET_REGION, BUCKET}, AWS:{QR_PATH}} = environment()
+    return `https://${BUCKET}.s3.${BUCKET_REGION}.amazonaws.com/${QR_PATH}${NAME}`
+
+}
