40b366e9af561813d408cdb82c92fa584f2ca6cb
jsburbano <jhoan@aplazo.mx>
2022-12-30 20:28:39 -0500

    fix: solved CORS

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: ['https://jessi-joan.vercel.app', 'http://localhost:3000'],
-  });
+  app.enableCors({ allowedHeaders: '*', origin: '*', credentials: true });
   await app.listen(3001);
 }
 bootstrap();
